9af42370106c22c9aa1a87c0bae2b94ce0493a8c
ian <ian.laffey@mail.mcgill.ca>
2023-10-14 16:04:24 -0700

    wip

--- inference/summary_get.py
+++ inference/summary_get.py
@@ -10,10 +10,10 @@ from transcription_get import get_transcription
 BASE_URL = "https://api.twelvelabs.io/v1.2"
 api_key = "tlk_25RFF6T069MJFF286MZEM3GC5KRY"
 index_id = "652afdd53c4a426cf3f4f541"
-video_id = "652afe5a43e8c47e4eb48057"
+video_id = "652b180143e8c47e4eb48080"
 data = {
     "video_id": video_id,
-    "prompt": "describe each shot in order. active voice. max detail. bulleted list."
+    "prompt": "with all of the information you have available, create a list of all scenes.."
 }
 
 
@@ -34,19 +34,24 @@ class Scene(BaseModel):
 
 @ai_model
 class Scenes(BaseModel):
+    '''Take into account the movie and the context when generating the scenes. Provide additional detail when possible.'''
+    movie_name: str
     scene_list:List[Scene]
 
-scenes = Scenes(response.text)
+scenes = Scenes(response.text, movie_name="Iron Man")
 
 print(scenes)
 
 transcription = get_transcription(index_id, video_id)
 
-video_name = "Apple Think Different"
+video_name = "Iron Man"
 
-system_prompt = f"You will be given a list of shots in a video, and a transcription of the audio in the video. It is your job to map the shots in the video to timestamps in the video. The transcription will be a list of text and timestamps The timestamps are formatted as floats representing seconds. The transcription audio is not something we have control over. The list of shots will be a list of descriptions of the visual imagery on screen. The goal is to find empty time slots, where no one is talking- and describe what is happening in the scene. Please output the timestamps and narration, where we can safely narrate. Please try and find time slots that are long enough for the human to narrate the given text"
+system_prompt = f"""You will be given a list of shots in a video, and a transcription of the audio in the video. It is your job to map the shots in the video to timestamps in the video. The transcription will be a list of text and timestamps. The transcription audio is not something we have control over. The list of shots will be a list of descriptions of the visual imagery on screen. The goal is to find empty time slots, where no one is talking- and describe what is happening in the scene. Please output the timestamps and narration, where we can safely narrate. Please try and find time slots that are long enough for the human to narrate the given text.
 
-user_prompt = f"""Video Name: {str(video_name)},
+Please supplement the descriptions with relevant details given the context of the video."""
+
+user_prompt = f"""Video Name: {str(video_name)}
+Video Length: 2 minutes, 21 seconds
 Transcription: {str(transcription)}
 Scene Description: {str(scenes)}"""
 
