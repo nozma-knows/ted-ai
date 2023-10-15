54b35d638b6e5d16090b778fa6287b98c0abdbeb
ian <ian.laffey@mail.mcgill.ca>
2023-10-15 00:05:28 -0700

    update scene_gen

--- anime/backend/summary_get.py
+++ anime/backend/summary_get.py
@@ -4,7 +4,8 @@ from marvin import ai_fn, ai_model, ChatCompletion, settings
 from pydantic import BaseModel, Field
 from typing import Dict, List
 
-settings.llm_model="openai/gpt-4"
+settings.llm_model = "openai/gpt-4"
+
 
 def generate_scene(video_id: str) -> Dict:
     # Variables
@@ -16,7 +17,9 @@ def generate_scene(video_id: str) -> Dict:
         "prompt": "describe this video to a professional storyteller so they can make an anime out of it",
     }
 
-    response = requests.post(f"{BASE_URL}/generate", json=data, headers={"x-api-key": api_key})
+    response = requests.post(
+        f"{BASE_URL}/generate", json=data, headers={"x-api-key": api_key}
+    )
     return response.json()
 
 
@@ -24,21 +27,32 @@ class Character(BaseModel):
     name: str = Field(description="The name of the character.")
     description: str = Field(description="A description of the character.")
     imagery: str = Field(description="A description of the character's appearance.")
-    personality: str = Field(description="A description of the character's personality.")
+    personality: str = Field(
+        description="A description of the character's personality."
+    )
 
 
 @ai_model
 class Scene(BaseModel):
-    '''Create a dynamic scene and a list of relevant characters '''
+    """Create a dynamic scene and a list of relevant characters"""
 
     name: str = Field(description="The name of the scene.")
     imagery: str = Field(description="A description of the setting of the scene.")
-    plot: str = Field(description="An imaginary plot, based on the original but changed for dramatic effect. The plot should draw from anime storytelling conventions and be engaging for the user. Be creative! Make sure to use events, characters and settings from the original video whenever possible!")
-    characters: List[Character] = Field(description="An imaginative description of characters that could be in the scene. We will be generating an imaginary anime using these characters, that will be an interactive story. Please make sure that there are suitable characters for such an imaginary story. Inventing characters that make sense in the scene is OK. Please make sure that the characters are represented by humans or humanoids and avoid anthropomorphized animals or objects.")
+    music: str = Field(
+        description="A description of the music that plays in the scene."
+    )
+    plot: str = Field(
+        description="An imaginary plot, based on the original but changed for dramatic effect. The plot should draw from anime storytelling conventions and be engaging for the user. Be creative! Make sure to use events, characters and settings from the original video whenever possible!"
+    )
+    characters: List[Character] = Field(
+        description="An imaginative description of characters that could be in the scene. We will be generating an imaginary anime using these characters, that will be an interactive story. Please make sure that there are suitable characters for such an imaginary story. Inventing characters that make sense in the scene is OK. Please make sure that the characters are represented by humans or humanoids and avoid anthropomorphized animals or objects."
+    )
+
 
 def vid2scene(video_id: str) -> List[Scene]:
     desc_from_twelve_labs = generate_scene(video_id)
     return Scene(str(desc_from_twelve_labs))
 
-space_x="652b5c1b43e8c47e4eb4829b"
+
+space_x = "652b5c1b43e8c47e4eb4829b"
 # print(vid2scene(space_x))
