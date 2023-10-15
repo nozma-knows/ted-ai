2060eb5276e83918a6349fcbab2878e3e89a917a
ian <ian.laffey@mail.mcgill.ca>
2023-10-14 20:03:12 -0700

    move to old

---
+++ old/inference/describe_shots_with_character_names.py
@@ -0,0 +1,55 @@
+descriptions = '''
+In this scene from Iron Man, Tony Stark is seen seated in a chair with a tablet, observing various scenes on the screen.
+In this Iron Man scene, Tony Stark is in the midst of an earthquake.
+In this scene from Iron Man, we see Tony Stark amongst a group of people engaged in different activities.
+In this shot of Iron Man, we see Tony Stark, absorbed in something on a television screen.
+In this scene from Iron Man, we see Tony Stark, the man, holding an object and observing it.
+In this scene of the movie Iron Man, the main character, Tony Stark, is seen running through rubble.
+In this Iron Man scene, Tony Stark is depicted as standing up and looking to his left side while wearing a black jacket.
+In this Iron Man scene, we see Tony Stark sitting on a sofa, holding a remote control, and watching something on the screen while surrounded by other people.
+In the scene, Tony Stark, the main character, is positioned next to a poster of Iron Man hanging on one side.
+In this shot of Iron Man, we see Tony Stark, the main character, in a gym.
+In this scene from Iron Man, we see Tony Stark, referred as 'The man', engrossed in watching TV.
+In this shot from Iron Man, the main character, Tony Stark, is seen standing and observing his reflection.
+In this scene from Iron Man, Tony Stark is the man holding a boxing glove.
+In this scene from Iron Man, Tony Stark, referred to as 'The man', is standing next to a table.
+In this shot of the movie Iron Man, the main character Tony Stark appears as the man wearing an iron suit.
+In this Iron Man scene, Tony Stark, the main character, is seen holding a hammer.
+In this scene of Iron Man, Tony Stark, represented as 'The man', is watching something on television.
+In this shot from Iron Man, we see Tony Stark, the main character, at home lifting weights.
+In this scene from Iron Man, Tony Stark is seen throwing something downward from a building.
+In this shot from Iron Man, Tony Stark is engaging in an intense box-throwing session inside a house.
+In this Iron Man scene, Tony Stark is the man standing in front of a mirror and engaging in various activities within a room.
+In this scene from Iron Man, Tony Stark is shown watching television. He's holding an object and interacting with the screen using his hands.
+In this shot from Iron Man, Tony Stark, who is the man referred to in the scene, is throwing objects at the screen.
+In this shot from Iron Man, the main character Tony Stark is seen lifting weights with both hands.
+In the movie Iron Man, the shot is of Tony Stark, who is observed observing pictures on the wall.
+In this scene from Iron Man, Tony Stark is seen interacting with an image of himself.
+In this scene from Iron Man, Tony Stark, the man in a white t-shirt and black pants, is standing in an indoor gym
+In this shot from Iron Man, we see Tony Stark, the man, examining his body and discussing the results of some exercises.
+In this shot from Iron Man, we see Tony Stark in front of shelves filled with exercise equipment.
+In this shot from Iron Man, we see Tony Stark, the man, looking at something on his wristwatch while holding it up with one hand.
+In this scene of Iron Man, Tony Stark is depicted as the man lifting weights and performing various exercises with dumbbells.
+In this shot from Iron Man, Tony Stark is depicted as the man talking to someone else nearby in the gym.
+In this shot from Iron Man, we see Tony Stark, the man in various attires, engaging in different exercise routines.
+In this Iron Man scene, Tony Stark, the main character, is seen lifting weights, throwing water at himself, and performing exercises with dumbbells.
+In the scene from Iron Man, Tony Stark is depicted as the man jumping onto stairs.
+In this scene from Iron Man, we see Tony Stark, the man, breaking through a glass door.
+In this shot from Iron Man, Tony Stark is seen jumping onto a tiled floor.
+In this animated scene from Iron Man, we see Tony Stark, who is portrayed as a woman in black attire, walking on stairs.
+In this scene from Iron Man, Tony Stark, referred to as 'The woman', is seen encountering various objects and machines in motion.
+In this shot from Iron Man, Tony Stark is seen as the woman descending the staircase.
+In Iron Man, Tony Stark is seen in a shot where he is depicted as 'The woman', interacting with objects such as chains.
+In this shot from Iron Man, we see Tony Stark, referred to as 'The woman', lifting and dropping items from above onto the ground.
+In this shot from Iron Man, the main character, Tony Stark, is watching an advertisement for a show called 'Man's Athletic Journey'.
+In this scene from Iron Man, the main character, Tony Stark, depicted as a man in a black suit, is shown interacting with various objects and using tools.
+In this scene of Iron Man, Tony Stark, referred to as 'The man', is seen wearing a helmet and working on a robot or machine.
+In this scene of Iron Man, the main character Tony Stark is seen engaging in different activities and interactions with other individuals in suits or costumes.
+In the movie Iron Man, the main character Tony Stark, also known as Iron Man, is seen in action, dressed in his iconic red suit with a helmet and mask.
+In this scene from Iron Man, we see Tony Stark, also known as Iron Man, skillfully using various tools and machinery to disarm adversaries.
+In this shot from Iron Man, we see Tony Stark, also known as Iron Man, placing an iron object on his face.
+In this scene from Iron Man, we see Tony Stark, also known as Iron Man, wearing glasses while lifting another piece of equipment onto his head.
+In this scene from Iron Man, Tony Stark, dressed as Iron Man, is standing among various statues and images of himself.
+In this shot from Iron Man, Tony Stark, the man in the scene, is pointing at the screen and speaking about his athletic journey.
+In this animated scene, Tony Stark, dressed as Iron Man, interacts with other characters in their costumes.
+In this shot from Iron Man, the camera zooms in on a specific photo while Tony Stark discusses its content.'''
