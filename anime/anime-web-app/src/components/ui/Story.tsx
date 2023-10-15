c12ca0e45bfbea9ca9b27fb223a12cb9ab3ec785
ian <ian.laffey@mail.mcgill.ca>
2023-10-15 03:51:06 -0700

    working image display

--- anime/anime-web-app/src/components/ui/Story.tsx
+++ anime/anime-web-app/src/components/ui/Story.tsx
@@ -10,7 +10,9 @@ interface PanelData {
   text: string;
 }
 
-const Story: FC<StoryProps> = ({ video, character, scene }:) => {
+const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
+
+const Story: FC<StoryProps> = ({ video, character, scene }) => {
   const [prompt, setPrompt] = useState<string>("");
   const [showInput, setShowInput] = useState<boolean>(false);
   const [messageCount, setMessageCount] = useState<number>(0);
@@ -23,92 +25,62 @@ const Story: FC<StoryProps> = ({ video, character, scene }:) => {
     setPrompt("");
   };
 
-  const fetchNextPanel = async (scene: Scene): Promise<Panel> => {
-    const response = await fetch('/api/next_panel/');
+  const fetchNextPanel = async (scene: Scene): Promise<PanelData> => {
+    const response = await fetch(`${backendUrl}/next_panel/`);
     const data = await response.json();
     const panel: Panel = data.response;
   
     // Find the character in the scene with the same name as the panel's character
     const character = scene.characters.find(c => c.name === panel.character);
   
-    // If the character is found, set the active image to the character's image
+    // If the character is found, return the panel data
     if (character) {
       if (!character.imageUrl) {
         throw new Error("Character image is not defined");
       }
-      setActivePanel(character.imageUrl);
+      return {
+        imageUrl: character.imageUrl,
+        characterName: character.name,
+        text: panel.text,
+      };
     }
   
-    return panel;
+    throw new Error("Character not found in scene");
   };
   
-  const fetchNextNarration = async (scene: Scene): Promise<Narration> => {
-    const response = await fetch('/api/next_narration/');
+  const fetchNextNarration = async (scene: Scene): Promise<PanelData> => {
+    const response = await fetch(`${backendUrl}/next_narration/`);
     const data = await response.json();
     const narration: Narration = data.response;
   
-    // If the narration's name is "Narrator", set the active image to the scene's image
+    // If the narration's name is "Narrator", return the narration data
     if (narration.name === "Narrator") {
       if (!scene.imageUrl) {
         throw new Error("Scene image is not defined");
       }
-      setActivePanel({
-        characterName: narration.name,
+      return {
         imageUrl: scene.imageUrl,
+        characterName: narration.name,
         text: narration.text,
-      });
+      };
     }
   
-    return narration;
+    throw new Error("Narration name is not 'Narrator'");
   };
-
-  // Define a type for the image data
-
-
 
 // Fetch initial narration and image when component mounts
 useEffect(() => {
-  fetchNextNarration(scene).then(narration => {
-    setActivePanel({
-      imageUrl: scene.imageUrl,
-      characterName: narration.name,
-      text: narration.text,
-    });
-    fetchNextPanel(scene).then(panel => {
-      const character = scene.characters.find(c => c.name === panel.character);
-      if (character) {
-        setNextPanel({
-          imageUrl: character.imageUrl,
-          characterName: panel.character,
-          text: panel.text,
-        });
-      }
-    });
-  });
-}, []); // Depend on whatever state variables you need
+  fetchNextNarration(scene).then(setActivePanel);
+  fetchNextPanel(scene).then(setNextPanel);
+}, [scene]);
 
-// Handle the next button click
-const handleNext = () => {
-  if (nextPanel) {
-    setActivePanel(nextPanel);
-    fetchNextPanel(scene).then(panel => {
-      const character = scene.characters.find(c => c.name === panel.character);
-      if (character) {
-        setNextPanel({
-          imageUrl: character.imageUrl,
-          characterName: panel.character,
-          text: panel.text,
-        });
-      }
-    });
-  }
-};
-
-// In your JSX
-<Button onClick={handleNext} colorScheme="teal">Next</Button>
-
-
-
+  const handleNext = () => {
+    if (nextPanel) {
+      setActivePanel(nextPanel);
+      setMessageCount(count => count + 1);
+      fetchNextPanel(scene).then(setNextPanel);
+    }
+  };
   const { music } = useMusicContext();
 
   return (
@@ -117,7 +89,7 @@ const handleNext = () => {
 
       <Flex w="full" h="full" bg="blackAlpha.400" rounded="md">
         Story View
-        <img src={activePanel} alt="Active" />
+        {activePanel && <img src={activePanel.imageUrl} alt="Active" />}
       </Flex>
 
       {showInput ? (
@@ -137,7 +109,7 @@ const handleNext = () => {
           </Flex>
         </form>
       ) : (
-        <Button colorScheme="teal">Next</Button>
+        <Button onClick={handleNext} colorScheme="teal">Next</Button>
       )}
     </Stack>
   );
